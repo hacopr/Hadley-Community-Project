a224c6ec996d7bb818a50bc294f940d1ed88371b
hacopr <johnsonj.2026@hadleyschools.org>
2026-03-02 13:04:18 -0500

    Add files via upload

--- Hadley Community Project/sidenav.js
+++ Hadley Community Project/sidenav.js
@@ -2,26 +2,31 @@ let calendar = "calendar.html";
 let organizations = "organizations.html";
 let resources = "resources.html";
 let about = "about.html";
-let lastSelection;
+let lastSelection = "none";
+let sidenavStatus = 0;
 
 function openSidenav(currentSelection) {
     document.getElementById("frame").src = currentSelection;
     document.getElementById("sidenav").style.left = "0px";
     document.getElementById("bulletin").style.left = "640px";
+    sidenavStatus = 1;
 }
 
 function operateSidenav(currentSelection) {
-    if (document.getElementById("sidenav").style.left == "-640px") {    //checks if sidenav is closed
-        openSidenav(currentSelection);                                  //opens sidenav
+    if (sidenavStatus == 0) {                                   //checks if sidenav is closed
+        openSidenav(currentSelection);
         }
     else {
         document.getElementById("sidenav").style.left = "-640px";       //closes sidenav
         document.getElementById("bulletin").style.left = "0px";
-            if (currentSelection != lastSelection) { //checks if selection is new
-                setTimeout(() => {openNav(currentSelection);}, 600);
-                }
+        sidenavStatus = 0;
+    }
+    if (lastSelection != currentSelection) {
+        setTimeout(() => {openSidenav(currentSelection);}, 600);
         }
+    
     lastSelection = currentSelection;
+    
 }
 
 
